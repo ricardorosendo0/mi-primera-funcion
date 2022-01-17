8dafc94816a3d58ac8690f7cf1b9424f494eed39
Ricardo Rosendo <31318399+ricardorosendo0@users.noreply.github.com>
2022-01-16 21:36:50 -0500

    Update wp-config.php
    Nuevos parametros definidos

--- wp-config.php
+++ wp-config.php
@@ -11,3 +11,17 @@ RewriteRule ^(.*)$ https://%{HTTP_HOST}/$1 [R=301,L]
 define('SP_REQUEST_URL', ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']);
 define('WP_SITEURL', SP_REQUEST_URL);
 define('WP_HOME', SP_REQUEST_URL);
+
+
+/* Force SSL on Login Pages */ 
+define( 'FORCE_SSL_LOGIN', true ); 
+/* Force SSL for Admin Backend */ 
+define( 'FORCE_SSL_ADMIN', true );
+/* Set Maximum Number of Revisions. */ 
+define( 'WP_POST_REVISIONS', '10' );
+/* Compress All CSS Files */ 
+define( 'COMPRESS_CSS', true ); 
+/* Compress All JS Files */ 
+define( 'COMPRESS_SCRIPTS', true ); 
+/* Turn On GZIP Compression */ 
+define( 'ENFORCE_GZIP', true );
